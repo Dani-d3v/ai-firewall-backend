1abaffd39b9764c1ccbd228e28eeee58931028c3
YourGitHubUsername <your@email.com>
2026-03-24 13:30:58 +0300

    remove delete account function

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,22 +5,4 @@ const User = require("../models/user");
 
 exports.getUserProfile = asyncHandler(async (req, res) => sendSuccess(res, req.user));
 
-exports.deleteMyAccount = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
 
-  if (!user) {
-    const error = new Error("User not found");
-    error.statusCode = 404;
-    throw error;
-  }
-
-  await Payment.deleteMany({ userId: user._id });
-  await user.deleteOne();
-
-  return sendSuccess(res, {
-    deleted: true,
-    userId: req.user._id,
-  }, {
-    message: "User account deleted",
-  });
-});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,6 @@ const { protect } = require("../middleware/authMiddleware");
 
 // Protected route
 router.get("/profile", protect, getUserProfile);
-router.delete("/delete", protect, deleteMyAccount);
+
 
 module.exports = router;
